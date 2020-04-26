6bfdd927d5118d25d0cab6939ae80fcd682df5b8
Gabriel Mariano Marcelino <gabriel.mm8@gmail.com>
2020-04-25 23:01:38 -0300

    Adding a main header file

---
+++ include/cest/cest.h
@@ -0,0 +1,48 @@
+/*
+ * cest.h
+ * 
+ * Copyright (C) 2020, Gabriel Mariano Marcelino <gabriel.mm8@gmail.com>
+ * 
+ * This file is part of CEST library.
+ * 
+ * CEST library is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * CEST library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with CEST library. If not, see <http://www.gnu.org/licenses/>.
+ * 
+ */
+
+/**
+ * \brief Centroid Extractor for Star Trackers definition.
+ * 
+ * \author Gabriel Mariano Marcelino <gabriel.mm8@gmail.com>
+ * 
+ * \version 0.1.0
+ * 
+ * \date 21/04/2020
+ * 
+ * \defgroup cest CEST
+ * \{
+ */
+
+#ifndef CEST_H_
+#define CEST_H_
+
+#include "centroid.hpp"
+#include "centroider.h"
+#include "star_filter.h"
+#include "star_filter_hw.h"
+#include "star_filter_sw.h"
+#include "star_pixel.hpp"
+
+#endif // CEST_H_
+
+//! \} End of cest group
